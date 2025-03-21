f098b8aeafa187ac838c9db5ee15b007713aff7e
Alban Kerloc'h <alban.kerloch@gmail.com>
2025-03-21 16:07:53 +0000

    better use of dates

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def load_data():
         password=password,
         database='diary'
     )
-    query="SELECT DISTINCT date, duree_totale, brossette FROM diary.raw_data where date >= STR_TO_DATE('01/03/2025', '%d/%m/%Y') "
+    query="SELECT DISTINCT date, duree_totale, brossette FROM diary.raw_data where date >= DATE_FORMAT(NOW(), '%Y-%m-01')"
     df = pd.read_sql(query, conn)
     df['duree_heure'] = df['duree_totale'].dt.total_seconds() / 3600 
     conn.close()
@@ -41,33 +41,14 @@ data = load_data()
 data_load_state.text("")
 
 data_brossette = data[['date', 'brossette']]
-data_brossette.set_index('date', inplace=True)
+data_brossette['date'] = pd.to_datetime(data_brossette['date']).dt.strftime('%Y-%m-%d')
 
 fig = px.line(data, x='date', y='duree_heure', labels={'date': 'Date', 'duree_heure': 'Travail (heures)'}, title='Travail par jour')
-
-# Afficher le graphique
 st.plotly_chart(fig)
 
-# Afficher le calendrier
-fig_brossette, ax = calmap.calendarplot(data_brossette['brossette'], 
-    daylabels=['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'],
-    monthlabels=['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novvembre', 'Décembre'],
-    cmap='YlGn', 
-    fig_kws={'figsize': (12, 4)})
+fig_brossette, ax = plt.subplots()
+july.month_plot(data_brossette['date'], data_brossette['brossette'], month=3, date_label=True, ax=ax, colorbar=True)
 st.pyplot(fig_brossette)
-
-## Create data
-dates_july = date_range("2025-03-01", "2025-03-20")
-data_july = np.random.randint(0, 100, len(dates_july))
-
-## Create a figure with a single axes
-fig, ax = plt.subplots()
-
-## Tell july to make a plot in a specific axes
-july.month_plot(dates_july, data_brossette['brossette'], month=3, date_label=True, ax=ax, colorbar=True)
-
-## Tell streamlit to display the figure
-st.pyplot(fig)
 
 if st.checkbox("Show raw data"):
     st.subheader("Raw data")
