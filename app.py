1abbbf93f9e35f7edbe4ffa9fe98620e4b04f1a9
Alban Kerloc'h <alban.kerloch@gmail.com>
2025-03-21 16:44:18 +0000

    calendar UI

--- app.py
+++ app.py
@@ -10,17 +10,13 @@ import numpy as np; np.random.seed(sum(map(ord, 'calmap')))
 import pandas as pd
 import matplotlib.pyplot as plt
 import july
-from july.utils import date_range
+from datetime import datetime
 
 
 load_dotenv()
 password=os.getenv('DATABASE_PASSWORD')
 
-HERE = os.path.dirname(os.path.abspath(__file__))
-
 st.title("Mes Heures")
-
-DATA = os.path.join(HERE, "data.csv")
 
 @st.cache_data
 def load_data():
@@ -32,20 +28,19 @@ def load_data():
     )
     query="SELECT DISTINCT date, duree_totale, brossette FROM diary.raw_data where date >= DATE_FORMAT(NOW(), '%Y-%m-01')"
     df = pd.read_sql(query, conn)
-    df['duree_heure'] = df['duree_totale'].dt.total_seconds() / 3600 
+    df['duree_heure'] = df['duree_totale'].dt.total_seconds() / 3600
     df['date'] = pd.to_datetime(df['date']).dt.strftime('%Y-%m-%d')
     conn.close()
     return df
 
-data_load_state = st.text("Loading data...")
-data = load_data()
-data_load_state.text("")
+with st.spinner('Chargement des données...'):
+    data = load_data()
 
 fig = px.line(data, x='date', y='duree_heure', labels={'date': 'Date', 'duree_heure': 'Travail (heures)'}, title='Travail par jour')
 st.plotly_chart(fig)
 
 fig_brossette, ax = plt.subplots()
-july.month_plot(data['date'], data['brossette'], month=3, date_label=True, ax=ax, colorbar=True)
+july.month_plot(data['date'], data['brossette'], cmap="github", weeknum_label=False, fontfamily="monospace", date_label=True, ax=ax)
 st.pyplot(fig_brossette)
 
 if st.checkbox("Show raw data"):
