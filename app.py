a12dc6dfe51a745f7ed15550cbb52375cfc11bb8
albankerloch <alban.kerloch@gmail.com>
2025-06-24 20:29:51 +0200

    add rerun

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import july
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 load_dotenv()
 password=os.getenv('DATABASE_PASSWORD')
@@ -23,41 +24,50 @@ st.markdown(style2, unsafe_allow_html=True)
 st.title("Journ-Alban")
 st.markdown('</div>', unsafe_allow_html=True)
 
-start_date = datetime.now()
+
+if 'start_date' not in st.session_state:
+    st.session_state.start_date = datetime.now()
 
 col1, col2, col3 = st.columns(3)
 
 with col1:
     st.markdown('<div class="vertical-align">', unsafe_allow_html=True)
-    right = st.button("", icon=":material/arrow_back:", use_container_width=True)
+    if st.button("", icon=":material/arrow_back:", use_container_width=True):
+        st.session_state.start_date = st.session_state.start_date - relativedelta(months=1)
+        st.rerun()
     st.markdown('</div>', unsafe_allow_html=True)
 
 with col2:
-    st.header(datetime.now().strftime('%B %Y'))
+    st.header(st.session_state.start_date.strftime('%B %Y'))
 
 with col3:    
     st.markdown('<div class="vertical-align">', unsafe_allow_html=True)
-    left = st.button("", icon=":material/arrow_forward:", use_container_width=True)
+    if st.button(" ", icon=":material/arrow_forward:", use_container_width=True):
+        st.session_state.start_date = st.session_state.start_date + relativedelta(months=1)
+        st.rerun()
     st.markdown('</div>', unsafe_allow_html=True)
 
 @st.cache_data
-def load_data(start_date = datetime.now()):
-    start_date = start_date.strftime('%Y-%m-01')
+def load_data(startdate = datetime.now()):
+    enddate = startdate + relativedelta(months=1)
+    startdate = startdate.strftime('%Y-%m-01')
+    enddate = enddate.strftime('%Y-%m-01')
     conn = mysql.connector.connect(
         host='diary-database.c1igk0esk62c.eu-west-3.rds.amazonaws.com',
         user='alban',
         password=password,
         database='diary'
     )
-    query=f"SELECT DISTINCT date, duree_totale, brossette, manger FROM diary.raw_data where date >= '{start_date}'"
-    df = pd.read_sql(query, conn, params={"start_date": start_date})
-    df['duree_heure'] = df['duree_totale'].dt.total_seconds() / 3600
-    df['date'] = pd.to_datetime(df['date']).dt.strftime('%Y-%m-%d')
+    query=f"SELECT DISTINCT date, duree_totale, brossette, manger FROM diary.raw_data where date >= '{startdate}' and date < '{enddate}'"
+    df = pd.read_sql(query, conn)
+    if len(df) != 0:
+        df['duree_heure'] = df['duree_totale'].dt.total_seconds() / 3600
+        df['date'] = pd.to_datetime(df['date']).dt.strftime('%Y-%m-%d')
     conn.close()
     return df
 
 with st.spinner('Chargement des données...'):
-    data = load_data(start_date)
+    data = load_data(startdate = st.session_state.start_date)
 
 fig = px.bar(data, x='date', y='duree_heure', labels={'date': 'Date', 'duree_heure': 'Travail (heures)'}, title='')
 fig.update_xaxes(
